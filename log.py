6db4565bc614debd9ae859a2ebcf31db8b838f79
Caelan Chapman <cchap.mkb@protonmail.com>
2018-03-19 06:10:09 -0400

    Creates a log which includes information about the song as well as a timestamp.

---
+++ log.py
@@ -0,0 +1,23 @@
+import parser
+import csv
+import os.path
+from time import localtime, strftime
+
+# Check if file exists, if not then declare the first row as an identifying row
+if os.path.exists("history.csv") == False:
+    with open('history.csv', 'a') as csvfile:
+        logger = csv.writer(csvfile, dialect='excel')
+        parameters = "Timestamp", "Song", "Artist"
+        logger.writerow(parameters)
+
+# appends song information to logfile
+def appendLog():
+
+    timeString = strftime("%Y-%m-%d %H:%M:%S", localtime())         #creates the string containing the date and time
+    songInformation = parser.getSongData()                          #fetches the current song data
+    parameters = timeString, songInformation[0], songInformation[1]
+
+
+    with open('history.csv', 'a') as csvfile:           #open the log file as csvfile
+        logger = csv.writer(csvfile, dialect='excel')   #create/open the log buffer as logger
+        logger.writerow(parameters)                     #log relevant data to file
