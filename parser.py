e5effbbff2d132d13951a7dd5f78ce6dd79df620
Caelan Chapman <cchap.mkb@protonmail.com>
2018-03-19 06:08:33 -0400

    Isolated into function.

--- parser.py
+++ parser.py
@@ -1,20 +1,19 @@
 import os
 
-os.system("rm 087.html")
-os.system("wget muzakwpn.muzak.com/wpn/087.html")
-with open("087.html") as website:
-    docLines = website.readlines()
-    trackLine = docLines[15]
-    trackToSplit = trackLine[50:]
-    track = trackToSplit.split("<")
-    currentTrack = track[0]
-    currentTrack = currentTrack.split(", by ")
-    currentSong = currentTrack[0]
-    currentArtist = currentTrack[1]
+# wget website and extract song title/artist.
+def getSongData():
 
-'''
-os.system("clear")
-os.system("cls")
-print("Song: ", currentSong)
-print("Artist: ", currentArtist)
-'''
+    os.system("rm 087.html")
+    os.system("wget muzakwpn.muzak.com/wpn/087.html")
+
+    with open("087.html") as website:
+        docLines = website.readlines()
+        trackLine = docLines[15]
+        trackToSplit = trackLine[50:]
+        track = trackToSplit.split("<")
+        currentTrack = track[0]
+        currentTrack = currentTrack.split(", by ")
+        currentSong = currentTrack[0]
+        currentArtist = currentTrack[1]
+        songInformation = currentSong, currentArtist
+        return(songInformation)
