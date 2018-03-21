fba6dcbf1bb513d62afa8d80242b6eddef752df8
Dan Fiscus <dafiscus@gmail.com>
2018-03-21 02:03:48 -0400

    Got the Spotify API call working!! Also added a sample response file.

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ import log
 import handler
 import time
 import os
+import platform
 
 def main():
     # this is a test change to see how gpg signed commits work...
@@ -12,7 +13,7 @@ def main():
     handler.authRoutine()
     songInformationCurrent = fetch.getCurrentSong()
     songInformationOld = songInformationCurrent
-    #os.system("clear")
+    screenRefresh()
     print("Current song playing: ", songInformationCurrent[0], ", by", songInformationCurrent[1])
     print("Attempting to fetch track metadata from Spotify...")
     handler.metaFetch(songInformationCurrent[0],songInformationCurrent[1])
@@ -27,11 +28,21 @@ def main():
         if songInformationCurrent != songInformationOld:
             log.appendLog()
             songInformationOld = songInformationCurrent
-            os.system("clear")
-            print("Current song playing: ", songInformationCurrent[0], ", by", songInformationCurrent[1])
-
+            screenRefresh()
+            #print("Current song playing: ", songInformationCurrent[0], ", by", songInformationCurrent[1])
+            handler.metaFetch(songInformationCurrent[0],songInformationCurrent[1])
         # pulls data from website every 5 seconds
         time.sleep(5)
+
+def screenRefresh():
+    #clear the command line screen using the appropriate command based on the user's operating system
+    if platform.system() == "Linux" or platform.system() == "Darwin":
+        os.system("clear")
+    elif platform.system() == "Windows":
+        os.system("cls")
+    else:
+        print("wtf lol")
+
 
 # I don't really understand why this works but it does
 if __name__ == "__main__":
